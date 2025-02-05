8db10183a58d0e377bb812abf12047710d25dbd2
Brenner Resende Borges <brenner.resende@rocketmail.com>
2025-02-05 09:02:45 -0300

    Criando entity Post com User aninhado

---
+++ src/main/java/com/brennerresende/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.brennerresende.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.brennerresende.workshopmongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String>{
+
+}
