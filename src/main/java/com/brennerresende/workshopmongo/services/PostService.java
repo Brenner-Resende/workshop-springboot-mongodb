a85397cda0831ca42200cdf42966b49a05f3407b
Brenner Resende Borges <brenner.resende@rocketmail.com>
2025-02-05 09:27:55 -0300

    Obtendo post por id

---
+++ src/main/java/com/brennerresende/workshopmongo/services/PostService.java
@@ -0,0 +1,22 @@
+package com.brennerresende.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.brennerresende.workshopmongo.domain.Post;
+import com.brennerresende.workshopmongo.repository.PostRepository;
+import com.brennerresende.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
